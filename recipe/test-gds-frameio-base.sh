e26c7a060ce2a0a9efe520571f37024062512823
Duncan Macleod <duncan.macleod@ligo.org>
2021-06-03 14:28:08 +0100

    add functional tests
    in a test script

---
+++ recipe/test-gds-frameio-base.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# IGWN Conda Distribution tests for gds-frameio-base
+#
+
+set -ex
+
+# use framecpp_sample to generate some fake data
+framecpp_sample --description IGWN_CONDA_TEST --start-time 1000000000
+GWF="Z-IGWN_CONDA_TEST-1000000000-1.gwf"
+
+# print frame segments
+fdir ${GWF}
+
+# print frame info
+finfo ${GWF}
+
+# dump ascii
+fextract Z0:RAMPED_INT_2U_1 ${GWF} data.txt
+
+# dump frame
+framedump ${GWF}
+
+# set time
+fsettime .
